5a547c5e8eecd150bf10bef9b54d2f56d1e2fb4b
Alexei Pancratov <apancratov@endava.net>
2016-04-08 17:33:58 +0300

    Added the Book entity

---
+++ OnlineLibrary/OnlineLibrary.DataAccess/Entities/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.DataAccess.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public string FrontCover { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
