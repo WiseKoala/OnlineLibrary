9b7e216aede7072eea4b1bb42a5f1d23e9738dd0
Alexandru Dascal <dascalalexandru@gmail.com>
2016-04-08 17:29:25 +0300

    Fixed issue #10 and issue #13

--- OnlineLibrary/OnlineLibrary.DataAccess/Entities/Role.cs
+++ OnlineLibrary/OnlineLibrary.DataAccess/Entities/Role.cs
@@ -10,6 +10,6 @@ namespace OnlineLibrary.DataAccess.Entities
     public class Role : IdentityRole
     {
         public Role() : base() { }
-        public Role(string name) : base(name) { }
+        public Role(string name) : base(name) { }       
     }
 }
