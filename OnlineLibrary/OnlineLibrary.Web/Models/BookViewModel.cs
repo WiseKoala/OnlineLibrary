5a0dcf757a5915bb61eb0c3744d5266a80b8c547
Alexei Pancratov <apancratov@endava.net>
2016-04-11 13:46:30 +0300

    Added BookViewModel

---
+++ OnlineLibrary/OnlineLibrary.Web/Models/BookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLibrary.Web.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
