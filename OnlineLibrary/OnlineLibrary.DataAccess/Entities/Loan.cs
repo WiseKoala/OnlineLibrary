9cc2d13d99261532fa4400958879a8a3ca2804cc
Alexei Pancratov <apancratov@endava.net>
2016-04-18 16:11:44 +0300

    Added the Loan entity and LoanStatus enum

---
+++ OnlineLibrary/OnlineLibrary.DataAccess/Entities/Loan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using OnlineLibrary.DataAccess.Enums;
+
+namespace OnlineLibrary.DataAccess.Entities
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(BookCopy))]
+        public int BookCopyId { get; set; }
+        public LoanStatus Status { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        public BookCondition BookConditionAtReturn { get; set; }
+
+        public User User { get; set; }
+        public BookCopy BookCopy { get; set; }
+    }
+}
