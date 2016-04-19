86f96a7e94dd3d9079502eacef1e38ea5e68b861
aschiopu <Alexandru.Schiopu@endava.com>
2016-04-19 15:23:55 +0300

    BookDetailsController code review

--- OnlineLibrary/OnlineLibrary.Web/Controllers/BookDetailsController.cs
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/BookDetailsController.cs
@@ -14,17 +14,23 @@ namespace OnlineLibrary.Web.Controllers
 {
     public class BookDetailsController : BaseController
     {
+        // CR: the following declarations look weird to me
+        // CR: This should be declared within method scope and rewritten as 
+        // CR: string[] arr = Enumerable.Repeat(string.Empty, Enum.GetValues(typeof(BookCondition)).Length).ToArray();
         private string[] conditionStrArray = { "", "", "", "", "", "" };
+        // CR: This should be declared within method scope
         private string conditionStr = "";
 
         public ActionResult Index(int id)
         {
+            // CR: something is wrong here
             if (!IsUserNameSessionVariableSet())
             {
                 InitializeUserNameSessionVariable();
             }
 
             var book = DbContext.Books.Include(b => b.BookCopies).First(b => b.Id == id);
+            // CR: this is not used
             var bookcopies = new List<BookCopy>();
             int[] condition =
             {
@@ -36,6 +42,7 @@ namespace OnlineLibrary.Web.Controllers
                 (DbContext.BookCopies.Where(n => n.BookId == id && n.Condition == DataAccess.Enums.BookCondition.Poor).Count())
             };
 
+            // CR: bad ident
                 if (condition[0] != 0) { conditionStrArray[0] = condition[0] + " New"; };
                 if (condition[1] != 0) { conditionStrArray[1] = condition[1] + " Fine"; };
                 if (condition[2] != 0) { conditionStrArray[2] = condition[2] + " Very Good"; };
@@ -43,9 +50,21 @@ namespace OnlineLibrary.Web.Controllers
                 if (condition[4] != 0) { conditionStrArray[4] = condition[4] + " Fair"; };
                 if (condition[5] != 0) { conditionStrArray[5] = condition[5] + " Poor"; };
 
+            //CR: bad formating
             foreach (var s in conditionStrArray)
             { if (s.Length != 0) { conditionStr = conditionStr + s + ", "; } };
            if (conditionStr.Length > 3) { conditionStr = conditionStr.Substring(0, conditionStr.Length - 2); };
+            
+            //I would rewrite the above code by:
+          /*var conditionStr = DbContext.BookCopies.GroupBy(e => e.Condition)
+            .Select(e => string.Concat("GetConditionDescription(e.Key)", " ", e.Count())).ToList()
+            .Aggregate((current, next) => string.Concat(current, ", ", next));
+
+            Or using database dictionary for Condition:
+            var conditionStr = DbContext.BookCopies.Include(b => b.Condition).GroupBy(e => e.Condition)
+                .Select(e => string.Concat(e.Key.Name, " ", e.Count())).ToList()
+                .Aggregate((current, next) => string.Concat(current, ", ", next));
+            */
 
             // Obtain list of not available book copies.
             IEnumerable<BookCopy> notAvailableBookCopies = from bc in book.BookCopies
