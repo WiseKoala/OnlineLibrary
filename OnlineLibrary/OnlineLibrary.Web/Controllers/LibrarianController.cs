1ead55d8ffc486a544bea512ce256c3c4652a01a
alexandrudascal <dascalalexandru@gmail.com>
2016-04-19 21:53:33 +0300

    Added initial librarian's controller and view

---
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/LibrarianController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineLibrary.Web.Controllers
+{
+    public class LibrarianController : Controller
+    {
+        // GET: Librarian
+        [Authorize(Roles = "Librarian, SysAdmin, SuperAdmin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
