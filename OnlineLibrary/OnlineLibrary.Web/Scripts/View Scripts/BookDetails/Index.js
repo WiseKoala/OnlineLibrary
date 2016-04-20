04ba0f2400794eaa6689f7965febd9a770227cb7
Ioana Ianovici <iianovici@endava.net>
2016-04-20 14:10:06 +0300

    Improved design Solved git merge issue

--- OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/BookDetails/Index.js
+++ OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/BookDetails/Index.js
@@ -5,35 +5,6 @@
 
             url: '/BookDetails/CreateLoanRequest/',
 
-<<<<<<< Updated upstream
-        request.done(function () {
-            // Implemented toaster functionality with success message
-            toastr.options =
-                {
-                    "closeButton": true,
-                    "onclick": null,
-                    "positionClass": "toast-bottom-right",
-                    "timeOut": 30000,           // 30 seconds
-                    "extendedTimeOut": 30000    // another 30 seconds, if user hovers mouse over notification
-                }
-            toastr.success('Please wait for a confirmation from our librarian before coming to the library to pick up the book.', 'Loan Request Sent')
-            // alert("success");
-        });
-
-        request.fail(function (jqXHR, textStatus) {
-            // Implemented toaster functionality with error message
-            toastr.options =
-                {
-                    "closeButton": true,
-                    "onclick": null,
-                    "positionClass": "toast-bottom-right",
-                    "timeOut": 30000,           // 30 seconds
-                    "extendedTimeOut": 60000    // another 30 seconds, if user hovers mouse over notification
-                }
-            toastr.error('Please try one more time or contact our library\'s system administrator if the error persists.', 'Error Sending Loan Request')
-            // alert("fail");
-        });
-=======
             data: { 'id': $('#loan').data("book-id") },
 
             success: function (response) {
@@ -69,7 +40,6 @@
 
                 }
             },
->>>>>>> Stashed changes
 
             error: function (response) {
                 // The url or controller was not found.
@@ -87,4 +57,7 @@
             }
         });
     });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
 });
