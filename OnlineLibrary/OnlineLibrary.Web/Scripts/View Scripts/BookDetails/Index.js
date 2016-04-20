1ed97d6e1c198568be784a9a93f64ff21e49a802
Ioana Ianovici <iianovici@endava.net>
2016-04-20 13:33:01 +0300

    Improved error detecting

--- OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/BookDetails/Index.js
+++ OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/BookDetails/Index.js
@@ -1,11 +1,11 @@
 ﻿$(document).ready(function () {
     $('#loan').click(function () {
 
-        var request = $.ajax({
-            url: '/BookDetails/CreateLoanRequest/',
-            data: { 'id': $('#loan').data("book-id") }
-        });
+        $.ajax({
 
+            url: '/BookDetails/CreateLoanRequest/',
+
+<<<<<<< Updated upstream
         request.done(function () {
             // Implemented toaster functionality with success message
             toastr.options =
@@ -33,6 +33,58 @@
             toastr.error('Please try one more time or contact our library\'s system administrator if the error persists.', 'Error Sending Loan Request')
             // alert("fail");
         });
+=======
+            data: { 'id': $('#loan').data("book-id") },
 
+            success: function (response) {
+
+                if (response.error != null)
+                {
+                    // There was an error in the controller.
+
+                    toastr.options =
+                        {
+                            "closeButton": true,
+                            "onclick": null,
+                            "positionClass": "toast-bottom-right",
+                            "timeOut": 30000,           // 30 seconds
+                            "extendedTimeOut": 60000    // another 30 seconds, if user hovers mouse over notification
+                        }
+                    toastr.error('Please try one more time or contact our library\'s system administrator if the error persists.', 'Error Sending Loan Request')
+
+                }
+                else
+                {
+                    // The controller returned with success.
+
+                    toastr.options =
+                        {
+                            "closeButton": true,
+                            "onclick": null,
+                            "positionClass": "toast-bottom-right",
+                            "timeOut": 30000,           // 30 seconds
+                            "extendedTimeOut": 30000    // another 30 seconds, if user hovers mouse over notification
+                        }
+                    toastr.success('Please wait for a confirmation from our librarian before coming to the library to pick up the book.', 'Loan Request Sent')
+
+                }
+            },
+>>>>>>> Stashed changes
+
+            error: function (response) {
+                // The url or controller was not found.
+
+                toastr.options =
+                        {
+                            "closeButton": true,
+                            "onclick": null,
+                            "positionClass": "toast-bottom-right",
+                            "timeOut": 30000,           // 30 seconds
+                            "extendedTimeOut": 60000    // another 30 seconds, if user hovers mouse over notification
+                        }
+                toastr.error('Please try one more time or contact our library\'s system administrator if the error persists.', 'Error Sending Loan Request')
+
+            }
+        });
     });
 });
