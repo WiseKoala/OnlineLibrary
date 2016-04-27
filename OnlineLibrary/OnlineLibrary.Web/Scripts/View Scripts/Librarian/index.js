1cefc3c8f0c9a3ca750e908d048285fc69ebab02
Alexei Pancratov <apancratov@endava.net>
2016-04-27 14:36:55 +0300

    Moved JS code from the view to the index.js file

--- OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/Librarian/index.js
+++ OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/Librarian/index.js
@@ -1,13 +1,38 @@
 ﻿$( document ).ready(function() {
     $(".approve").click(function () {
         var loanId = $(this).data('loanId');
-        $("#approve").find('input[id="loanId"]').val(loanId);
+        $("#approveForm").find('input[id="loanId"]').val(loanId);
     });
-});
 
-$(document).ready(function () {
     $(".reject").click(function () {
         var loanId = $(this).data('loanId');
         $("#reject").find('input[id="loanId"]').val(loanId);
+    });
+
+    $("#approveLoanButton").click(function () {
+        var approveForm = $("#approveForm");
+        var settings = {
+            method: "POST",
+            url: approveForm.find('input[id="approveUrl"]').val(),
+            data: {
+                bookCopyId: approveForm.find('input[id="bookCopyId"]').val(),
+                loanId: approveForm.find('input[id="loanId"]').val()
+            }
+        }
+        // Make AJAX request.
+        $.ajax(settings)
+            .done(function (response) {
+                if (response.error) {
+                    $("#approveLoanErrorAlert").removeClass("hidden");
+                    $("#approveLoanError").text(response.error);
+                }
+                else {
+                    window.location.replace(approveForm.find('input[id="redirectUrl"]').val());
+                }
+
+                //return false;
+            });
+
+        return false;
     });
 });
