704c7d47e38559a55388751fafda3842e833629c
Alexei Pancratov <apancratov@endava.net>
2016-05-04 13:21:16 +0300

    Extracted the JS code to a separate file

--- OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/Librarian/index.js
+++ OnlineLibrary/OnlineLibrary.Web/Scripts/View Scripts/Librarian/index.js
@@ -1,4 +1,5 @@
-﻿$( document ).ready(function() {
+﻿$(document).ready(function () {
+
     $(".approve").click(function () {
         var loanId = $(this).data('loanId');
         $("#approveForm").find('input[id="loanId"]').val(loanId);
@@ -35,4 +36,63 @@
 
         return false;
     });
+
+    // ===== Tabs functionality.
+    function LoansViewModel() {
+        this.pending = ko.observableArray([]);
+        this.approved = ko.observableArray([]);
+        this.inProgress = ko.observableArray([]);
+        this.history = ko.observableArray([]);
+    }
+
+    // Activate knockout.js
+    var viewModel = new LoansViewModel();
+    ko.applyBindings(viewModel);
+
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        // Extract the selected tab.
+        var tabAnchor = $(e.target);
+
+        // Initialize the settings object.
+        var settings = {};
+        settings.type = "GET";
+        settings.complete = function (jqXHR) {
+            // Find active tab.
+            var tabName = $("ul#loans-tabs > li.active > a").attr("href");
+            switch (tabName) {
+                case "#pending":
+                    viewModel.pending(jqXHR.responseJSON);
+                    break;
+                case "#approved":
+                    viewModel.approved(jqXHR.responseJSON);
+                    break;
+                case "#inProgress":
+                    viewModel.inProgress(jqXHR.responseJSON);
+                    break;
+                case "#history":
+                    viewModel.history(jqXHR.responseJSON);
+                    break;
+                default:
+                    console("Error");
+                    break;
+            }
+        };
+
+        // Determine the URL to make request to.
+        var requestUrl = tabAnchor.data("requestUrl");
+        if (tabAnchor.attr("href") === "#history") {
+            settings.url = requestUrl;
+        }
+        else {
+            var status = tabAnchor.data("status");
+            settings.url = requestUrl + "?status=" + status;
+        }
+
+        // Make the AJAX request to the server.
+        $.ajax(settings);
+    });
+
+    // Trigger the event manually in order to load the tab content.
+    $('a[data-toggle="tab"]:first').trigger("show.bs.tab");
 });
