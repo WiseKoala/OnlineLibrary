d0f52dab7cd7e4261f7a9e0d964120ccccc33c47
Ioana Ianovici <iianovici@endava.net>
2016-05-10 09:30:18 +0300

    fixed issue #80

--- OnlineLibrary/OnlineLibrary.Web/Controllers/RoleController.cs
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/RoleController.cs
@@ -1,20 +1,16 @@
 ﻿using Microsoft.AspNet.Identity;
-using OnlineLibrary.DataAccess.Entities;
+using OnlineLibrary.DataAccess;
+using OnlineLibrary.DataAccess.Abstract;
+using OnlineLibrary.Services.Concrete;
 using OnlineLibrary.Web.Infrastructure.Abstract;
-using OnlineLibrary.Web.Models;
-using System.Collections.Generic;
+using OnlineLibrary.Web.Models.RoleViewModels;
 using System.Data.Entity;
 using System.Linq;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using OnlineLibrary.DataAccess;
-using Microsoft.Owin.Security;
-using OnlineLibrary.Services.Concrete;
-using System.Web;
 using Microsoft.AspNet.Identity.EntityFramework;
-using OnlineLibrary.DataAccess.Abstract;
-using OnlineLibrary.DataAccess.Concrete;
+using System.Collections.Generic;
 
 namespace OnlineLibrary.Web.Controllers
 {
@@ -37,41 +33,25 @@ namespace OnlineLibrary.Web.Controllers
             {
                 InitializeUserNameSessionVariable();
 
-                // Initializing the model to be passed to the view.
                 var model = new RoleViewModel();
 
-                // Initializing model components.
-                List<IdentityRole> roles = _roleService.GetRoleList();
-                model.Roles = roles;
+                model.AllRoles = _roleService.GetRoleList();
+                model.UsersAndTheirRoles = new List<UserAndRole>();
 
-                List<User> users = _userService.GetUserList();
-                model.UserNames = new List<string>();
-
-                // Creating a temporary variable to store the information for the model.
-                string userNames;
-
-                // Iterating through the roles to get the usernames for each role.
-                foreach (var role in roles)
+                foreach (var user in _userService.GetUserList())
                 {
-                    if (role.Users == null || !role.Users.Any())
-                    {
-                        // Show a message if there are no users in a role.
-                        userNames = "No users have this role.";
-                    }
-                    else
-                    {
-                        // Get all users in this role.
-                        var allUsersInRole = role.Users.Select(userInRole => userInRole.UserId);
+                    string userName = DbContext.Users.Where(u => u.Id == user.Id).Single().UserName;
 
-                        // Get the usernames list for the users in this role.
-                        var userNamesList = users.Where(user => allUsersInRole.Contains(user.Id)).Select(user => user.UserName);
+                    string roleId = user.Roles.Single().RoleId;
 
-                        // Join the usernames list as a string.
-                        userNames = string.Join(", ", userNamesList);
-                    }
-                    
-                    // Add the information about usernames to the model.
-                    model.UserNames.Add(userNames);
+                    string roleName = DbContext.Roles.Where(r => r.Id == roleId).Single().Name;
+
+                    model.UsersAndTheirRoles.Add(
+                        new UserAndRole
+                        {
+                            UserName = userName,
+                            RoleName = roleName
+                        });
                 }
 
                 return View(model);
@@ -80,39 +60,39 @@ namespace OnlineLibrary.Web.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        public async Task<ActionResult> Edit(string id)
-        {
-            if (HasAdminPrivileges(User))
-            {
-                Role role = await _roleService.FindByIdAsync(id);
-                string[] memberIDs = role.Users.Select(x => x.UserId).ToArray();
-                IEnumerable<User> members = _userService.Users.Where(x => memberIDs.Any(y => y == x.Id));
-                IEnumerable<User> nonMembers = _userService.Users.Where( u => u.UserName != "Admin").Except(members);
-
-                return View(new RoleEditModel
-                {
-                    Role = role,
-                    Members = members,
-                    NonMembers = nonMembers
-                });
-            }
-            return RedirectToAction("Index", "Home");
-        }
-
         [HttpPost]
-        public async Task<ActionResult> Edit(RoleModificationModel model)
+        public async Task<ActionResult> Index(string userName, string roleName)
         {
             if (HasAdminPrivileges(User))
             {
                 if (ModelState.IsValid)
                 {
-                    if (await EditUsersRoles(model))
-                        return RedirectToAction("Index");
-                    else
-                        return View("Error");
+                    var user = DbContext.Users
+                                        .Include(u => u.Roles)
+                                        .Where(u => u.UserName == userName).SingleOrDefault();
+
+                    if (user != null)
+                    {
+                        string roleId = user.Roles.Single().RoleId;
+
+                        var oldRoleName = DbContext.Roles.Where(r => r.Id == roleId).Single().Name;
+
+                        if (await _userService.RemoveUserFromRole(user.Id, oldRoleName) && await _userService.AddUserToRole(user.Id, roleName))
+                        {
+                            return RedirectToAction("Index", "Role");
+                        }
+                        else
+                        {
+                            return View("Error", new string[] { "Role or User was Not Found" });
+                        }
+                    }
+
+                    return View("Error", new string[] { "User was Not Found" });
                 }
-                return View("Error", new string[] { "Role Not Found" });
+
+                return View("Error", new string[] { "Role or User was Not Found" });
             }
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -122,69 +102,7 @@ namespace OnlineLibrary.Web.Controllers
         {
             return IsFirstLogin() || user.IsInRole(UserRoles.SysAdmin) || user.IsInRole(UserRoles.SuperAdmin);
         }
-
-        private async Task<bool> RemoveUserCurrentRoles(string userId)
-        {
-            var currentUserRoles = await _userService.GetRolesAsync(userId);
-            IdentityResult result = await _userService.RemoveFromRoleAsync(userId, currentUserRoles.Single());
-
-            return result.Succeeded;
-        }
-
-        private async Task<bool> AddUserToRole(string userId, RoleModificationModel model)
-        {
-            bool removeResult = await RemoveUserCurrentRoles(userId);
-            IdentityResult addResult = await _userService.AddToRoleAsync(userId, model.RoleName);
-
-            return removeResult && addResult.Succeeded;
-        }
-
-        private async Task<bool> RemoveUserFromRole(string userId, RoleModificationModel model)
-        {
-            IdentityResult removeResult = await _userService.RemoveFromRoleAsync(userId, model.RoleName);
-            IdentityResult addResult = await _userService.AddToRoleAsync(userId, UserRoles.User);
-
-            return removeResult.Succeeded && addResult.Succeeded;
-        }
-
-        private async Task<bool> EditUsersRoles(RoleModificationModel model)
-        {
-            // Change roles.
-            foreach (string userId in model.IdsToAdd ?? new List<string>())
-            {
-                if (!await AddUserToRole(userId, model))
-                {
-                    return false;
-                }
-            }
-            foreach (string userId in model.IdsToDelete ?? new List<string>())
-            {
-                if (!await RemoveUserFromRole(userId, model))
-                {
-                    return false;
-                }
-            }
-
-            // If current user has changed his/her role.
-            if (model.IdsToAdd.Contains(User.Identity.GetUserId()) ||
-                model.IdsToDelete.Contains(User.Identity.GetUserId()))
-            {
-                // Save current user name.
-                var currentUserName = User.Identity.Name;
-
-                // Sign the user out.
-                AuthenticationManager.SignOut();
-
-                // Sign the user back.
-                var currentUser = _userService.FindByName(currentUserName);
-                var identity = await _userService.CreateIdentityAsync(
-                    currentUser, DefaultAuthenticationTypes.ApplicationCookie);
-                AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = true }, identity);
-            }
-
-            return true;
-        }
-
+        
         public bool IsFirstLogin()
         {
             bool isFirstUserLogin = false;
---
+++ OnlineLibrary/OnlineLibrary.Web/Models/RoleViewModels/UserAndRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using OnlineLibrary.DataAccess.Entities;
+
+namespace OnlineLibrary.Web.Models.RoleViewModels
+{
+    public class UserAndRole
+    {
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
