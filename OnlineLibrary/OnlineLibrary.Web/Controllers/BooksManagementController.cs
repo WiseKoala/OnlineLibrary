311c3b8a2b9ca2f72c2df4b449eb16eb968c9f79
Ioana Ianovici <iianovici@endava.net>
2016-05-11 09:15:08 +0300

    Added controller, empty view and a view model

---
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/BooksManagementController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineLibrary.Web.Controllers
+{
+    public class BooksManagementController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
