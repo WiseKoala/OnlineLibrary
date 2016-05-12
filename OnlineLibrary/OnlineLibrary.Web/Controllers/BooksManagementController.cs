8ea63a4adedffbc1a1685c4c7df0a3d98b62b837
Alexandru Dascal <dascalalexandru@gmail.com>
2016-05-12 17:06:02 +0300

    Implemented CreateEdit View

--- OnlineLibrary/OnlineLibrary.Web/Controllers/BooksManagementController.cs
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/BooksManagementController.cs
@@ -1,20 +1,86 @@
-﻿using OnlineLibrary.Web.Models.BooksManagement;
+﻿using OnlineLibrary.DataAccess.Abstract;
+using OnlineLibrary.DataAccess.Enums;
+using OnlineLibrary.Web.Infrastructure.Abstract;
+using OnlineLibrary.Web.Models.BooksManagement;
+using OnlineLibrary.Web.Models.BooksManagement.CreateEditBookViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace OnlineLibrary.Web.Controllers
 {
-    public class BooksManagementController : Controller
+    public class BooksManagementController : BaseController
     {
+        public BooksManagementController(ILibraryDbContext dbContext)
+            :base(dbContext) {}
+
         public ActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public ActionResult CreateEdit(int bookId)
+        public ActionResult CreateEdit(int id = 2)
         {
+            var model = DbContext.Books.Where(b => b.Id == id)
+                                       .Include(b => b.SubCategories)
+                                       .Include(b => b.BookCopies)
+                                       .Select(m => new CreateEditBookViewModel
+                                       {
+                                           Id = m.Id,
+                                           Title = m.Title,
+                                           ISBN = m.ISBN,
+                                           Description = m.Description,
+                                           PublishDate = m.PublishDate,
+                                           FrontCover = m.FrontCover,
+                                          BookCopies = m.BookCopies.Select(bc => new BookCopyViewModel
+                                          {
+                                              Id = bc.Id,
+                                              BookCondition = bc.Condition
+                                          }).ToList(),
+                                          SubCategories = m.SubCategories.Select(sc => new SubCategoryViewModel
+                                          {
+                                              Id = sc.Id,
+                                              Name = sc.Name
+                                          }).ToList()
+                                      })
+                                       .SingleOrDefault();
 
-            return View();
+           // Create the book if doesn't exist.
+           if (model == null)
+           {
+               model = new CreateEditBookViewModel();
+           }
+
+            
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult CreateEdit( CreateEditBookViewModel model )
+        {
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult ListBookConditions()
+        {
+            var bookConditionNames = Enum.GetNames(typeof(BookCondition));
+
+            var dict = new Dictionary<string, int>();
+            foreach (var name in Enum.GetNames(typeof(BookCondition)))
+            {
+                dict.Add(name, (int)Enum.Parse(typeof(BookCondition), name));
+            }
+            var bookConditions = dict
+                .Select(kvp => new { Value = kvp.Value, Name = kvp.Key })
+                .ToList();
+
+            return Json(bookConditions, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ OnlineLibrary/OnlineLibrary.Web/Models/BooksManagement/CreateEditBookViewModels/CreateEditBookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Web;
+
+namespace OnlineLibrary.Web.Models.BooksManagement.CreateEditBookViewModels
+{
+    public class CreateEditBookViewModel
+    {
+        public CreateEditBookViewModel()
+        {
+            SubCategories = new List<SubCategoryViewModel>();
+            BookCopies = new List<BookCopyViewModel>();        
+        }
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public string FrontCover { get; set; }
+
+        public HttpPostedFileBase Image { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+        
+        public  List<SubCategoryViewModel> SubCategories { get; set; }
+        public  List<BookCopyViewModel> BookCopies { get; set; }
+
+    }
+}
