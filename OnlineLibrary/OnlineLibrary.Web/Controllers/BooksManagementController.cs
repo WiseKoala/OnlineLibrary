05215c1d4ef9c4d244106cada7ff0df817b5a794
Alexei Pancratov <apancratov@endava.net>
2016-05-13 11:31:03 +0300

    Added logic to save book and book copies to the CreateEdit POST action method

--- OnlineLibrary/OnlineLibrary.Web/Controllers/BooksManagementController.cs
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/BooksManagementController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using OnlineLibrary.Common.Exceptions;
 using OnlineLibrary.DataAccess.Abstract;
@@ -100,6 +102,104 @@ namespace OnlineLibrary.Web.Controllers
         }
 
         [HttpPost]
+        public ActionResult CreateEdit(CreateEditBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // If book is new.
+            if (model.Id < 1)
+            {
+                // Create new book.
+                var book = new Book()
+                {
+                    Id = model.Id,
+                    Title = model.Title,
+                    Description = model.Description,
+                    ISBN = model.ISBN,
+                    PublishDate = model.PublishDate,
+                    FrontCover = SaveImage(model.Image)
+                };
+
+                // Add book copies.
+                foreach (var bookCopy in model.BookCopies)
+                {
+                    book.BookCopies.Add(new BookCopy
+                    {
+                        Condition = bookCopy.BookCondition
+                    });
+                }
+
+                // Save book.
+                DbContext.Books.Add(book);
+                DbContext.SaveChanges();
+            }
+            else
+            {
+                // Update book.
+                Book book = DbContext.Books.Find(model.Id);
+                book.Title = model.Title;
+                book.Description = model.Description;
+                book.ISBN = model.ISBN;
+                book.FrontCover = SaveImage(model.Image);
+                book.PublishDate = model.PublishDate;
+
+                // Update book copies.
+                foreach (var bookCopyModel in model.BookCopies)
+                {
+                    BookCopy bookCopy = DbContext.BookCopies.Find(bookCopyModel);
+                    
+                    if (bookCopy != null)
+                    {
+                        // Update existing book copy.
+                        bookCopy.Condition = bookCopyModel.BookCondition;
+                    }
+                    else
+                    {
+                        // Create and add new book copy.
+                        BookCopy newBookCopy = new BookCopy()
+                        {
+                            Condition = bookCopyModel.BookCondition
+                        };
+                        book.BookCopies.Add(newBookCopy);
+                    }
+                }
+
+                DbContext.SaveChanges();
+            }
+
+            return RedirectToAction("CreateEdit", new { id = model.Id });
+        }
+
+        private string SaveImage(HttpPostedFileBase image)
+        {
+            string imageRelativeSavePath = null;
+
+            if (image != null)
+            {
+                var allowedContentTypes = new[]
+                {
+                    "image/jpeg", "image/png"
+                };
+
+                // If content type is allowed, save the image.
+                if (allowedContentTypes.Contains(image.ContentType))
+                {
+                    string contentPath = "~/Content/Images/Books/front-covers";
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                    string imageAbsoluteSavePath = Path.Combine(Server.MapPath(contentPath), fileName);
+                    image.SaveAs(imageAbsoluteSavePath);
+
+                    imageRelativeSavePath = string.Concat(contentPath, '/', fileName);
+                }
+            }
+
+            return imageRelativeSavePath;
+        }
+
+        [HttpPost]
         public ActionResult DeleteBook(int id)
         {
             Book removedBook = null;
@@ -120,12 +220,6 @@ namespace OnlineLibrary.Web.Controllers
             }
 
             return Json(removedBook, JsonRequestBehavior.DenyGet);
-        }
-
-        [HttpPost]
-        public ActionResult CreateEdit(CreateEditBookViewModel model)
-        {
-            return RedirectToAction("Index");
         }
 
         public JsonResult ListBookConditions()
