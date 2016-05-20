a536decdd5ecb757b73c4734f9e06628a4bbe37c
Alexandru Dascal <dascalalexandru@gmail.com>
2016-05-20 11:36:27 +0300

    Added Empty test class for IsBookAvailable method

---
+++ OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/IsBookAvailableTestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
+{
+    class IsBookAvailableTestClass
+    {
+    }
+}
