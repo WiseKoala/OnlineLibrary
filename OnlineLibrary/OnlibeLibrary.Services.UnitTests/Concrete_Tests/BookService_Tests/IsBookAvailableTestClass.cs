4bfb6eb6635b196aa772f4862014f85cda69386a
Alexandru Dascal <dascalalexandru@gmail.com>
2016-05-20 12:28:02 +0300

    Added Test Method

--- OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/IsBookAvailableTestClass.cs
+++ OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/IsBookAvailableTestClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
 {
+    [TestFixture]
     class IsBookAvailableTestClass
     {
+        [Test]
+        public void IsBookCopyRemovable_BookCopyisLaoned_ExpectFalse()
+        {
+
+        }
     }
 }
