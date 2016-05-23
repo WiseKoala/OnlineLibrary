c62d6421260d22818a059cebf4031e77736d2592
Alexandru Dascal <dascalalexandru@gmail.com>
2016-05-23 11:45:49 +0300

    Implemented Test Class for BookService.IsBookAvailable method

--- OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/IsBookAvailableTestClass.cs
+++ OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/IsBookAvailableTestClass.cs
@@ -1,19 +1,98 @@
-﻿using NUnit.Framework;
-using System;
+﻿using NSubstitute;
+using NUnit.Framework;
+using OnlineLibrary.DataAccess.Abstract;
+using OnlineLibrary.DataAccess.Entities;
+using OnlineLibrary.DataAccess.Enums;
+using OnlineLibrary.Services.Concrete;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
 {
     [TestFixture]
     class IsBookAvailableTestClass
     {
-        [Test]
-        public void IsBookCopyRemovable_BookCopyisLaoned_ExpectFalse()
-        {
+        private ILibraryDbContext _dbContext;
 
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var bookCopies = new List<BookCopy>
+                {
+                    new BookCopy { Id = 1, Condition = BookCondition.Fine },
+                    new BookCopy { Id = 2, Condition = BookCondition.VeryGood },
+                }
+           .AsQueryable();
+
+            var loans = new List<Loan>
+                {
+                    new Loan { BookCopyId = 1, Status = LoanStatus.Pending },
+                }
+            .AsQueryable();
+
+            var bookCopiesSet = Substitute.For<DbSet<BookCopy>, IQueryable<BookCopy>>();
+            ((IQueryable<BookCopy>)bookCopiesSet).Provider.Returns(bookCopies.Provider);
+            ((IQueryable<BookCopy>)bookCopiesSet).Expression.Returns(bookCopies.Expression);
+            ((IQueryable<BookCopy>)bookCopiesSet).ElementType.Returns(bookCopies.ElementType);
+            ((IQueryable<BookCopy>)bookCopiesSet).GetEnumerator().Returns(bookCopies.GetEnumerator());
+
+            var loansSet = Substitute.For<DbSet<Loan>, IQueryable<Loan>>();
+            ((IQueryable<Loan>)loansSet).Provider.Returns(loans.Provider);
+            ((IQueryable<Loan>)loansSet).Expression.Returns(loans.Expression);
+            ((IQueryable<Loan>)loansSet).ElementType.Returns(loans.ElementType);
+            ((IQueryable<Loan>)loansSet).GetEnumerator().Returns(loans.GetEnumerator());
+
+            _dbContext = Substitute.For<ILibraryDbContext>();
+            _dbContext.BookCopies.Returns(bookCopiesSet);
+            _dbContext.Loans.Returns(loansSet);
+        }
+
+        [Test]
+        public void IsBookAvailable_Should_RetrunFalse_When_BookCopyIsLoaned()
+        {
+            // Arrange.           
+            var expectedResult = false;
+            var sut = new BookService(_dbContext);          
+            // Book with status pending
+            var bookCopyId = 1;
+            
+            // Act.
+            var returnedResult = sut.IsBookCopyRemovable(bookCopyId);
+
+            // Assert.
+            Assert.AreEqual(expectedResult, returnedResult);
+        }
+
+        [Test]
+        public void IsBookAvailable_Should_RetrunTrue_When_BookCopyIsNotLoaned()
+        {
+            // Arrange.
+            var expectedResult = true;
+            var sut = new BookService(_dbContext);
+            // Book wich is not loaned 
+            var bookCopyId = 2;
+
+            // Act.
+            var returnedResult = sut.IsBookCopyRemovable(bookCopyId);
+
+            // Assert.
+            Assert.AreEqual(expectedResult, returnedResult);
+        }
+
+        [Test]
+        public void IsBookAvailable_Should_ThrowKeyNotFoundException_When_BookCopyDoesntExist()
+        {
+            // Arrange.
+            var sut = new BookService(_dbContext);
+            // Book that doesn't exist
+            var bookCopyId = 3;
+
+            // Act.
+            var testDelegate = new TestDelegate(() => sut.IsBookCopyRemovable(bookCopyId));
+
+            // Assert.
+            Assert.Throws<KeyNotFoundException>(testDelegate);
         }
     }
 }
