3c8ddc12319acc3a3cbf749f84db379cf5fd2a93
Alexei Pancratov <apancratov@endava.net>
2016-05-23 15:00:07 +0300

    Added controller, action method and view

---
+++ OnlineLibrary/OnlineLibrary.Web/Controllers/CategoriesManagementController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineLibrary.DataAccess.Abstract;
+using OnlineLibrary.Web.Infrastructure.Abstract;
+using OnlineLibrary.Web.Models.CategoriesManagement;
+
+namespace OnlineLibrary.Web.Controllers
+{
+    public class CategoriesManagementController : BaseController
+    {
+        public CategoriesManagementController(ILibraryDbContext dbContext)
+            : base(dbContext)
+        { }
+
+        public ActionResult Index()
+        {
+            var model = DbContext.Categories
+                .Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ OnlineLibrary/OnlineLibrary.Web/Models/CategoriesManagement/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLibrary.Web.Models.CategoriesManagement
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
