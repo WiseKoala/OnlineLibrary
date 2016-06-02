50caf9aaf3dfede69e9b57f05aed8a88f25cd323
Alexandru Dascal <dascalalexandru@gmail.com>
2016-06-02 11:31:55 +0300

    Added CategoryNotFoundException & SubcategoryNotFoundException

---
+++ OnlineLibrary/OnlineLibrary.Common/Exceptions/CategoryNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OnlineLibrary.Common.Exceptions
+{
+
+    [Serializable]
+    public class CategoryNotFoundException : Exception
+    {
+        public CategoryNotFoundException()
+        {
+        }
+
+        public CategoryNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public CategoryNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected CategoryNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ OnlineLibrary/OnlineLibrary.Common/Exceptions/SubcategoryNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OnlineLibrary.Common.Exceptions
+{
+    [Serializable]
+    public class SubcategoryNotFoundException : Exception
+    {
+        public SubcategoryNotFoundException()
+        {
+        }
+
+        public SubcategoryNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public SubcategoryNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected SubcategoryNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
