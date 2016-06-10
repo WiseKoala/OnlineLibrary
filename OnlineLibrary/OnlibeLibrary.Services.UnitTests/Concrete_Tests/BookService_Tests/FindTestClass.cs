ba12500793aefbf57457f97d0aee5779d031226e
Alexei Pancratov <apancratov@endava.net>
2016-06-10 16:37:40 +0300

    Added a few unit tests

--- OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/FindTestClass.cs
+++ OnlineLibrary/OnlibeLibrary.Services.UnitTests/Concrete_Tests/BookService_Tests/FindTestClass.cs
@@ -147,12 +147,49 @@ namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
             // SubCategories
             var subCategories = new List<SubCategory>
             {
-                new SubCategory { Id = 1, Name = "C# Programming", Category = categories.First(c => c.Id == 1) },
-                new SubCategory { Id = 2, Name = "Java Programming", Category = categories.First(c => c.Id == 1) },
-                new SubCategory { Id = 3, Name = "Classics", Category = categories.First(c => c.Id == 2) },
-                new SubCategory { Id = 4, Name = "Historical", Category = categories.First(c => c.Id == 2) },
-                new SubCategory { Id = 5, Name = "Ancient Civilizations", Category = categories.First(c => c.Id == 3) },
-                new SubCategory { Id = 6, Name = "World", Category = categories.First(c => c.Id == 3) },
+                new SubCategory { Id = 1, Name = "C# Programming", CategoryId = 1, Category = categories.First(c => c.Id == 1), Books = new List<Book>
+                {
+                    new Book() {                                                       // Category: Computers & Technology; Subcategory: C# Programming;
+                                 Title = "Learn C# in One Day and Learn It Well: C# for Beginners with Hands-on Project (Learn Coding Fast with Hands-On Project) (Volume 3)",
+                                 ISBN = "1518800270",
+                                 FrontCover = "~/Content/Images/Books/front-covers/1518800270.jpg",
+                                 Authors = new List<Author>() { authors.First(a => a.Id == 1) },
+                                 Description = "If you are a beginning programmer wanting to learn C# .NET programming, this book is the perfect introduction. It is too basic for experienced programmers, but it is just perfect for someone just starting out with C# programming, since it is easy to follow and all the concepts are explained very well.",
+                                 PublishDate = new DateTime(2015,10,27)
+                               },
+                    new Book() {                                                       // Category: Computers & Technology; Subcategory: C# Programming;
+                                 Title = "The C# Player's Guide (2nd Edition)",
+                                 ISBN = "0985580127",
+                                 FrontCover = "~/Content/Images/Books/front-covers/0985580127.jpg",
+                                 Authors = new List<Author>() { authors.First(a => a.Id == 2) },
+                                 Description = "The C# Player's Guide (2nd Edition) is the ultimate guide for people starting out with C#, whether you are new to programming, or an experienced vet. This guide takes you from your journey's beginning, through the most challenging parts of programming in C#, and does so in a way that is casual, informative, and fun. This version of the book is updated for C# 6.0, .NET 4.6, and Visual Studio 2015.",
+                                 PublishDate = new DateTime(2015,9,22)
+                               },
+                    }
+                },
+                new SubCategory { Id = 2, Name = "Java Programming", CategoryId = 1, Category = categories.First(c => c.Id == 1), Books = new List<Book>
+                {
+                    new Book() {                                                       // Category: Computers & Technology; Subcategory: Java Programming;
+                         Title = "Java Concurrency in Practice 1st Edition",
+                         ISBN = "7678678676",
+                         FrontCover = "~/Content/Images/Books/front-covers/7678678676.jpg",
+                         Authors = new List<Author>() {
+                                                             authors.First(a => a.Id == 3),
+                                                             authors.First(a => a.Id == 4),
+                                                             authors.First(a => a.Id == 5),
+                                                             authors.First(a => a.Id == 6),
+                                                             authors.First(a => a.Id == 7),
+                                                             authors.First(a => a.Id == 8)
+                                                           },
+                         Description = "This book will give you a good grounding in concurrent programming in Java.",
+                         PublishDate = new DateTime(2006, 5, 19)
+                       }
+                    }
+                },
+                new SubCategory { Id = 3, Name = "Classics", CategoryId = 2, Category = categories.First(c => c.Id == 2) },
+                new SubCategory { Id = 4, Name = "Historical", CategoryId = 2, Category = categories.First(c => c.Id == 2) },
+                new SubCategory { Id = 5, Name = "Ancient Civilizations", CategoryId = 3, Category = categories.First(c => c.Id == 3) },
+                new SubCategory { Id = 6, Name = "World", CategoryId = 3, Category = categories.First(c => c.Id == 3) },
             }
             .AsQueryable();
 
@@ -208,9 +245,24 @@ namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
             ((IQueryable<Book>)booksSet).ElementType.Returns(books.ElementType);
             ((IQueryable<Book>)booksSet).GetEnumerator().Returns(books.GetEnumerator());
 
+            var categoriesSet = Substitute.For<DbSet<Category>, IQueryable<Category>>();
+            ((IQueryable<Category>)categoriesSet).Provider.Returns(categories.Provider);
+            ((IQueryable<Category>)categoriesSet).Expression.Returns(categories.Expression);
+            ((IQueryable<Category>)categoriesSet).ElementType.Returns(categories.ElementType);
+            ((IQueryable<Category>)categoriesSet).GetEnumerator().Returns(categories.GetEnumerator());
+
+            var subcategoriesSet = Substitute.For<DbSet<SubCategory>, IQueryable<SubCategory>>();
+            ((IQueryable<SubCategory>)subcategoriesSet).Provider.Returns(subCategories.Provider);
+            ((IQueryable<SubCategory>)subcategoriesSet).Expression.Returns(subCategories.Expression);
+            ((IQueryable<SubCategory>)subcategoriesSet).ElementType.Returns(subCategories.ElementType);
+            ((IQueryable<SubCategory>)subcategoriesSet).GetEnumerator().Returns(subCategories.GetEnumerator());
+            subcategoriesSet.Include(Arg.Any<string>()).Returns(subcategoriesSet);
+
             _dbContext = Substitute.For<ILibraryDbContext>();
             _dbContext.Authors.Returns(authorsSet);
             _dbContext.Books.Returns(booksSet);
+            _dbContext.Categories.Returns(categoriesSet);
+            _dbContext.SubCategories.Returns(subcategoriesSet);
         }
 
         [Test]
@@ -258,6 +310,60 @@ namespace OnlibeLibrary.Services.UnitTests.Concrete_Tests.BookService_Tests
             var model = new BookSearchServiceModel
             {
                 Description = "concurrent Java"
+            };
+
+            // Act.
+            var returnedResult = sut.Find(model);
+
+            // Assert.
+            Assert.AreEqual(expectedResult, returnedResult.Count());
+        }
+
+        [Test]
+        public void Should_ReturnOneBook_Given_PublishDate()
+        {
+            // Arrange.           
+            var expectedResult = 1;
+            var sut = new BookService(_dbContext);
+            var model = new BookSearchServiceModel
+            {
+                PublishDate = new DateTime(2015, 9, 22)
+            };
+
+            // Act.
+            var returnedResult = sut.Find(model);
+
+            // Assert.
+            Assert.AreEqual(expectedResult, returnedResult.Count());
+        }
+
+        [Test]
+        public void Should_ReturnOneBook_Given_CategoryId()
+        {
+            // Arrange.           
+            var expectedResult = 3;
+            var sut = new BookService(_dbContext);
+            var model = new BookSearchServiceModel
+            {
+                CategoryId = 1
+            };
+
+            // Act.
+            var returnedResult = sut.Find(model);
+
+            // Assert.
+            Assert.AreEqual(expectedResult, returnedResult.Count());
+        }
+
+        [Test]
+        public void Should_ReturnOneBook_Given_SubcategoryId()
+        {
+            // Arrange.           
+            var expectedResult = 2;
+            var sut = new BookService(_dbContext);
+            var model = new BookSearchServiceModel
+            {
+                SubcategoryId = 1
             };
 
             // Act.
