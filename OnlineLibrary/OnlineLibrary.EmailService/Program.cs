ed5eea1e98c98cdd3c95cdb78368a958b94326e0
Alexei Pancratov <apancratov@endava.net>
2016-06-20 18:14:02 +0300

    Implemented functionality to send an email

--- OnlineLibrary/OnlineLibrary.EmailService/Program.cs
+++ OnlineLibrary/OnlineLibrary.EmailService/Program.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,13 +22,42 @@ namespace OnlineLibrary.EmailService
 
             var loansSet = new DataSet();
 
-            var adapter = new SqlDataAdapter("SELECT Email FROM Loans l JOIN AspNetUsers u ON l.UserId = u.Id", connection);
+            var adapter = new SqlDataAdapter("SELECT Email FROM Loans l JOIN AspNetUsers u ON l.UserId = u.Id WHERE l.ExpectedReturnDate = GETDATE()", connection);
 
             SqlCommandBuilder cmdBldr = new SqlCommandBuilder(adapter);
 
             adapter.Fill(loansSet, "AspNetUsers");
 
+            for (int row = 0; row < loansSet.Tables[0].Rows.Count; row++)
+            {
+                string email = loansSet.Tables[0].Rows[row][0].ToString();
+
+                SendNotification(email);
+            }
+
             Console.ReadLine();
+        }
+
+        private static void SendNotification(string email)
+        {
+            using (MailMessage message = new MailMessage(ConfigurationManager.AppSettings["Email"], email))
+            {
+                message.Subject = ConfigurationManager.AppSettings["MessageSubject"];
+                message.Body = ConfigurationManager.AppSettings["MessageBody"];
+
+                int port = int.Parse(ConfigurationManager.AppSettings["HostPort"]);
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", port);
+                client.EnableSsl = bool.Parse(ConfigurationManager.AppSettings["EnableSsl"]);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Email"], 
+                    ConfigurationManager.AppSettings["Password"]);
+
+                client.Send(message);
+
+                Console.WriteLine("Message was sent successfully!");
+            }
         }
     }
 }
