90bdaa4b36076092fded561fc5e4803eb2960408
Ioana Ianovici <iianovici@endava.net>
2016-06-29 14:17:00 +0300

    Added author name trimming functionality when check for duplicate books

--- OnlineLibrary/OnlineLibrary.Services/Models/BookServiceModels/DuplicateAuthorServiceModel.cs
+++ OnlineLibrary/OnlineLibrary.Services/Models/BookServiceModels/DuplicateAuthorServiceModel.cs
@@ -1,9 +1,51 @@
-﻿namespace OnlineLibrary.Services.Models.BookServiceModels
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineLibrary.Services.Models.BookServiceModels
 {
     public class DuplicateAuthorServiceModel
     {
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+
+        private string trimName(string name)
+        {
+            if (name != null)
+            {
+                return Regex.Replace(name, @"[^A-Za-z]", string.Empty);
+            }
+
+            return name;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+
+            set
+            {
+                _firstName = trimName(value);
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return _middleName; }
+
+            set
+            {
+                _middleName = trimName(value);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+
+            set
+            {
+                _lastName = trimName(value);
+            }
+        }
     }
 }
